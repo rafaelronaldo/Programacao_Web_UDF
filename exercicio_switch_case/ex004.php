5a0e1cb991a89243fca1d3536bd7f82f4462ba26
Rafael Ronaldo <rafaelronaldo.19@gmail.com>
2025-09-22 21:19:12 -0300

    Exercicio Switch Case - ex004 - Prof

---
+++ exercicio_switch_case/ex004.php
@@ -0,0 +1,34 @@
+<form action="ex004.php" method="GET">
+    A<input type="number" name="a">
+    <select name="opcao">
+        <option value="som">( + )</option>
+        <option value="sub">( - )</option>
+        <option value="mul">( * )</option>
+        <option value="div">( / )</option>
+    </select>
+    B <input type="number" name="b">
+    <button type="submit">Enviar</button>
+</form>
+
+<?php
+$a = $_GET["a"];
+$b = $_GET["b"];
+$op = $_GET["opcao"];
+
+switch ($op) {
+    case"som":
+        $resultado = $a + $b;
+        print("$a + $b = $resultado");
+        break;
+    case"sub":
+        $resultado = $a - $b;
+        print("$a - $b = $resultado");
+        break;
+    case"mul":
+        $resultado = $a * $b;
+        print("$a * $b = $resultado");
+        break;
+    case"div":
+        $resultado = $a / $b;
+        print("$a / $b = $resultado");
+}
