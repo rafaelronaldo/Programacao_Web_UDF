7a3d16daa37ef666a05d6076a9afe4ddf0bd9d8d
Rafael Ronaldo <rafaelronaldo.dev@gmail.com>
2025-11-09 20:41:31 -0300

    Update cadastrar-marca.php

--- Projeto2122n/cadastrar-marca.php
+++ Projeto2122n/cadastrar-marca.php
@@ -2,13 +2,10 @@
 <form action="?page=salvar-marca" method="POST">
     <input type="hidden" name="acao" value="cadastrar">
     <div class="mb-3">
-        <label>Nome Marca
+        <label>Nome
             <input type="text" name="nome_marca" class="form-control">
         </label>
         <div class="mb-3">
             <button type="submit" class="btn btn-primary">Enviar</button>
         </div>
-    <div class="mb-3">
-        <button type="submit" class="btn btn-primary">Enviar</button>
-    </div>
 </form>
